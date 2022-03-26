8d7125e660383b05d65df1b8706f8eb48e2ec8f8
vndat00 <ngocdat1908@gmail.com>
2022-03-26 18:33:37 +0700

    add services section in main

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,28 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader);
 
 /*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalClose = document.querySelectorAll('.services__modal-close');
 
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal');
+}
+
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener('click', () => {
+        modal(i);
+    });
+});
+
+modalClose.forEach((mc, i) => {
+    mc.addEventListener('click', () => {
+        // modalViews.forEach((mv) => {
+        //     mv.classList.remove('active-modal');
+        // });
+        modalViews[i].classList.remove('active-modal');
+    });
+});
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
